1f7269a3ffcfb9d42dbd4d13a86313023af4e684
Georgio <georgetrueman64@gmail.com>
2024-11-25 00:04:08 +0300

    update actors container

--- finder_movie/movies_project/static/js/actors.js
+++ finder_movie/movies_project/static/js/actors.js
@@ -66,11 +66,11 @@ document.addEventListener("DOMContentLoaded", function () {
                     "beforeend",
                     `
                     <button class="carousel-control-prev" type="button" data-bs-target="#actorsCarousel" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="carousel-control-prev-icon" aria-hidden="true" style="transform: scale(0.86);"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#actorsCarousel" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="carousel-control-next-icon" aria-hidden="true" style="transform: scale(0.86);"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
                     `
