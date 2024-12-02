56af6e5f24d8dadd75b01c3a7f051ba348ed6b21
Georgio <georgetrueman64@gmail.com>
2024-12-02 20:24:15 +0300

    update accounts settings & urls

--- finder_movie/finder_movie/urls.py
+++ finder_movie/finder_movie/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies_project.urls')),
+    path('api/accounts/', include('accounts.urls')),
 ]
