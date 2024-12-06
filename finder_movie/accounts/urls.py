fff207bfe63eb5a6c135f81902d165a81f9cfe66
Georgio <georgetrueman64@gmail.com>
2024-12-06 07:09:04 +0300

    new profile.html update urls.py views.py custom.css

--- finder_movie/accounts/urls.py
+++ finder_movie/accounts/urls.py
@@ -6,4 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('profile/', views.profile_view, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
