1810dd0b345f4d922ea0470680e73a4503bc3873
Georgio <georgetrueman64@gmail.com>
2024-12-07 17:33:23 +0300

    new edit_profile

--- finder_movie/accounts/urls.py
+++ finder_movie/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('profile/', views.profile_view, name='profile'),
     path('logout/', views.logout_view, name='logout'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
--- finder_movie/accounts/views.py
+++ finder_movie/accounts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
-from .forms import CustomUserCreationForm, AuthenticationForm
+from .forms import CustomUserCreationForm, AuthenticationForm, UserProfileForm
 from .models import UserProfile
-
 
 def signup(request):
     if request.method == 'POST':
@@ -14,7 +13,6 @@ def signup(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
-
 
 def login_view(request):
     if request.method == 'POST':
@@ -27,14 +25,26 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
 
-
 @login_required
 def profile_view(request):
     user_profile = UserProfile.objects.get(user=request.user)
     return render(request, 'accounts/profile.html', {'profile': user_profile})
 
-
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('accounts:login')
+
+@login_required
+def edit_profile(request):
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:profile')
+    else:
+        form = UserProfileForm(instance=user_profile)
+
+    return render(request, 'accounts/edit_profile.html', {'form': form})
