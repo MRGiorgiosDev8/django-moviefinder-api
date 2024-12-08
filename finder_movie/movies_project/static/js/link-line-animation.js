69cc85d7c55c14bc84191ca979d54d31bc6a11b1
Georgio <georgetrueman64@gmail.com>
2024-12-08 23:57:40 +0300

    update link-line-animation base.html & profiles

--- finder_movie/movies_project/static/js/link-line-animation.js
+++ finder_movie/movies_project/static/js/link-line-animation.js
@@ -1,45 +1,91 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const profileLink = document.getElementById('profile-link');
+    const logoutLink = document.getElementById('logout-link');
     const signupLink = document.getElementById('signup-link');
     const loginLink = document.getElementById('login-link');
-    const signupLine = signupLink.querySelector('.nav-line');
-    const loginLine = loginLink.querySelector('.nav-line');
+    const profileLine = profileLink ? profileLink.querySelector('.nav-line') : null;
+    const logoutLine = logoutLink ? logoutLink.querySelector('.nav-line') : null;
+    const signupLine = signupLink ? signupLink.querySelector('.nav-line') : null;
+    const loginLine = loginLink ? loginLink.querySelector('.nav-line') : null;
 
     function getLineWidth(line) {
         const computedStyle = window.getComputedStyle(line);
         return computedStyle.getPropertyValue('--line-width');
     }
 
-    signupLink.addEventListener('mouseenter', () => {
-        const lineWidth = getLineWidth(signupLine);
-        gsap.to(signupLine, {
-            duration: 0.1,
-            width: lineWidth,
-            ease: "power4.out"
+    if (profileLink && profileLine) {
+        profileLink.addEventListener('mouseenter', () => {
+            const lineWidth = getLineWidth(profileLine);
+            gsap.to(profileLine, {
+                duration: 0.1,
+                width: lineWidth,
+                ease: "power4.out"
+            });
         });
-    });
 
-    signupLink.addEventListener('mouseleave', () => {
-        gsap.to(signupLine, {
-            duration: 0.1,
-            width: '0%',
-            ease: "power4.out"
+        profileLink.addEventListener('mouseleave', () => {
+            gsap.to(profileLine, {
+                duration: 0.1,
+                width: '0%',
+                ease: "power4.out"
+            });
         });
-    });
+    }
 
-    loginLink.addEventListener('mouseenter', () => {
-        const lineWidth = getLineWidth(loginLine);
-        gsap.to(loginLine, {
-            duration: 0.1,
-            width: lineWidth,
-            ease: "power4.out"
+    if (logoutLink && logoutLine) {
+        logoutLink.addEventListener('mouseenter', () => {
+            const lineWidth = getLineWidth(logoutLine);
+            gsap.to(logoutLine, {
+                duration: 0.1,
+                width: lineWidth,
+                ease: "power4.out"
+            });
         });
-    });
 
-    loginLink.addEventListener('mouseleave', () => {
-        gsap.to(loginLine, {
-            duration: 0.1,
-            width: '0%',
-            ease: "power4.out"
+        logoutLink.addEventListener('mouseleave', () => {
+            gsap.to(logoutLine, {
+                duration: 0.1,
+                width: '0%',
+                ease: "power4.out"
+            });
         });
-    });
+    }
+
+    if (signupLink && signupLine) {
+        signupLink.addEventListener('mouseenter', () => {
+            const lineWidth = getLineWidth(signupLine);
+            gsap.to(signupLine, {
+                duration: 0.1,
+                width: lineWidth,
+                ease: "power4.out"
+            });
+        });
+
+        signupLink.addEventListener('mouseleave', () => {
+            gsap.to(signupLine, {
+                duration: 0.1,
+                width: '0%',
+                ease: "power4.out"
+            });
+        });
+    }
+
+    if (loginLink && loginLine) {
+        loginLink.addEventListener('mouseenter', () => {
+            const lineWidth = getLineWidth(loginLine);
+            gsap.to(loginLine, {
+                duration: 0.1,
+                width: lineWidth,
+                ease: "power4.out"
+            });
+        });
+
+        loginLink.addEventListener('mouseleave', () => {
+            gsap.to(loginLine, {
+                duration: 0.1,
+                width: '0%',
+                ease: "power4.out"
+            });
+        });
+    }
 });
