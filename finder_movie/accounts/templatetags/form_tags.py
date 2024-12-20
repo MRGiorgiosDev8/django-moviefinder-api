2e7a8104ba1eff0de6d265c421d0e5634387c63f
Georgio <georgetrueman64@gmail.com>
2024-12-21 00:22:11 +0300

    updtae documentation

--- finder_movie/accounts/templatetags/form_tags.py
+++ finder_movie/accounts/templatetags/form_tags.py
@@ -4,4 +4,14 @@ register = template.Library()
 
 @register.filter(name='add_class')
 def add_class(field, css_class):
+    """
+    Фильтр для добавления CSS класса к полю формы.
+
+    Аргументы:
+    field -- поле формы, к которому нужно добавить класс
+    css_class -- CSS класс, который нужно добавить
+
+    Возвращает:
+    Поле формы с добавленным CSS классом.
+    """
     return field.as_widget(attrs={"class": css_class})
